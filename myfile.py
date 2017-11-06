5c47ec16fedf4e3e4ff629d727618a9db4287608
sachin123-123 <31612234+sachin123-123@users.noreply.github.com>
2017-11-06 16:13:33 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import smtplib#import smtp library
+import smtplib               #import smtp library
 s = smtplib.SMTP('smtp.gmail.com',587)
-s.ehlo()
-s.starttls()
+s.hello()
+s.starttls()                  #to get access to security
 s.login('username','password')#login details
-s.sendmail('from','to','msg')#add recievers email id abd nessage
+s.sendmail('from','to','msg') #add recievers email id abd message
 s.close()
